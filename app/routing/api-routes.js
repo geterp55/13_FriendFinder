3e394c1a973385642eb903f9c612e5a50264ac97
Peter Guerrero <geterp@hotmail.com>
2016-07-24 14:47:20 -0500

    commit project

---
+++ app/routing/api-routes.js
@@ -0,0 +1,49 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources. 
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+
+//Your api-routes.js file should contain two routes:
+
+//A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+//A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+
+
+var friendArray = require('../data/friends.js');
+var path			= require('path');
+
+// ROUTING
+
+module.exports = function(app){
+
+
+	app.get('/api/friends', function(req, res){
+		console.log(req.body('friendArray.friendName'));
+		var friendName = req.body('friendArray.friendName');
+		var friendEmail = req.body('friendArray.friendEmail');
+		// var friendID = req.body('friendID');
+		// var phoneNumber = req.body('phoneNumber');
+
+		res.json(friendName + friendEmail);
+		// res.json(friendName + friendEmail + friendID + phoneNumber);	
+			
+		});
+
+	app.post('/api/friends', function(req, res){
+		var friendName = req.body.friendArray.friendName;
+		var friendEmail = req.body.friendArray.friendEmail;
+		// var friendID = req.body.friendID;
+		// var phoneNumber = req.body.phoneNumber;
+
+		res.json(friendName + friendEmail);
+		res.json(friendName + friendEmail + friendID + phoneNumber);
+
+	})
+
+
+
+}
+
---
+++ app/routing/html-routes.js
@@ -0,0 +1,35 @@
+//Your html-routes.js file should include two routes:
+
+//A GET Route to /survey which should display the survey page.
+//A default USE route that leads to home.html which displays the home page.
+
+//DEPENDENCIES
+var path = require('path');
+
+
+
+//ROUTING
+
+module.exports = function(app) {
+
+	// app.get('/', function(req, res){
+	// res.sendFile(path.join(__dirname, 'home.html'));
+	// })
+
+	app.get('/home', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey2.html'));
+	});
+
+
+	
+
+
+
+
+}
+
+	
